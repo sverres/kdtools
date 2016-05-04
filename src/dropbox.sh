9cebde660d8bfbc23ff328d9107c9929067cf0ec
Sverre Stikbakke <sverre.stikbakke@gmail.com>
2016-05-04 10:20:02 +0200

    Upgraded to Dropbox API V2

--- src/dropbox.sh
+++ src/dropbox.sh
@@ -5,17 +5,21 @@
 # sverre.stikbakke@ntnu.no 03.05.2016
 #
 
-DROPBOX_URL='https://content.dropboxapi.com/1/files_put/auto'
+DROPBOX_URL_API_V2='https://content.dropboxapi.com/2/files/upload'
 DROPBOX_TOKEN="your_dropbox_api_token"
 
 dropbox_upload () {
   UPLOAD_FILE="${1}"
 
-  curl -X PUT\
-  "${DROPBOX_URL}/${LOG_FOLDER}/${UPLOAD_FILE}"\
+  curl -X POST  "${DROPBOX_URL_API_V2}"\
+  --header "Authorization: Bearer ${DROPBOX_TOKEN}"\
+  --header "Dropbox-API-Arg: {\
+    \"path\": \"/${LOG_FOLDER}/${UPLOAD_FILE}\",\
+    \"mode\": \"overwrite\",\
+    \"autorename\": false,\
+    \"mute\": true}"\
+  --header "Content-Type: application/octet-stream"\
   --data-binary @"${LOG_FOLDER}/${UPLOAD_FILE}"\
-  -H 'Content-Type: text/plain'\
-  -H "Authorization: Bearer ${DROPBOX_TOKEN}"\
   >"$(basename ${UPLOAD_FILE} .txt).json"\
   2>/dev/null
 }
