7cca7cf880542fbc434c9b72d7e9b8d914489173
Sverre Stikbakke <sverre.stikbakke@gmail.com>
2016-12-27 11:48:14 +0100

    monthlycopy.sh function

--- src/monthlycopy.sh
+++ src/monthlycopy.sh
@@ -4,24 +4,34 @@
 #
 # run by cron the 1st of every month
 #
-# sverre.stikbakke@ntnu.no 01.11.2016
+# sverre.stikbakke@ntnu.no 27.12.2016
 #
 
 cd ./Dropbox 2>/dev/null
 
-LOG_FOLDER='isp-ip-address'
+LOG_FOLDERS='isp-ip-address ping-test speed-test'
 YESTERDAY_YEAR=$(date --date='1 days ago' +%Y)
-YESTERDAY_MONTH=$(date --date='1 days ago' +%m)
+YESTERDAY_MONTH=$(date --date='28 days ago' +%m)
 
 source ./dropbox.sh
 
-mkdir -p "${LOG_FOLDER}"
+move_to_archive () {
+    LOG_FOLDER="${1}"
 
-cd "${LOG_FOLDER}"
+    mkdir -p "${LOG_FOLDER}"
 
-mkdir -p "${YESTERDAY_YEAR}"
+    cd "${LOG_FOLDER}"
 
-echo "abc" >> $(date --date='1 days ago' +%Y-%m-)dill.txt
+    mkdir -p "${YESTERDAY_YEAR}"
 
-# sample command: mv 2016-10-ip-adress-time-txt 2016
-mv  "${YESTERDAY_YEAR}-${YESTERDAY_MONTH}-"* "${YESTERDAY_YEAR}"
+    echo "abc" >> $(date --date='28 days ago' +%Y-%m-)dill.txt
+
+    # sample command: mv 2016-10-ip-adress-time-txt 2016
+    mv  "${YESTERDAY_YEAR}-${YESTERDAY_MONTH}-"* "${YESTERDAY_YEAR}"
+
+    cd ..
+}
+
+for FOLDER in $LOG_FOLDERS; do
+    move_to_archive "${FOLDER}"
+done
